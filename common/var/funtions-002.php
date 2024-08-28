bd19eab95cd77640f70e73248228d4ed978668a7
rustyhaze2010 <110825165+rustyhaze2010@users.noreply.github.com>
2024-08-28 08:05:41 -0500

    Update funtions-002.php
    Signed-off-by: rustyhaze2010 <110825165+rustyhaze2010@users.noreply.github.com>

--- common/var/funtions-002.php
+++ common/var/funtions-002.php
@@ -3,7 +3,6 @@
 include_once "timekeeper.php"; 		// keeps time variables	
 include_once "variables.php";
 
-
 function fireImageSEARCH($movname, $title){
     $dognap=("+art+history+slides+period");
     
@@ -14,15 +13,13 @@ function fireImageSEARCH($movname, $title){
     echo "<a href='https://duckduckgo.com/?t=ffab&q=" . $movname . $dognap . "' target='_parent'><br>Searchs***</a>";
     echo "<a href='https://en.wikipedia.org/w/index.php?search=" . $movname . $dognap . "' target='_parent'><br>Wikis**</a>";
     echo "<a href='https://archive.org/details/image?tab=collection&query=" . $movname . $dognap . "' target='_parent'><br> Articles*</a>";
-    
-	echo "<br><a href='index.php?page=artSearch-levelone' target='_parent'>back</a>";
+    echo "<br><a href='index.php?page=artSearch-levelone' target='_parent'>back</a>";
 
-#  " . $movname . $dognap . "
+#  	" . $movname . $dognap . "
 #	<br>*** = DuckDuckGo.com
 #	<br>** = Wikipedia.org
-#  <br>* = Archive.org  
+#  	<br>* = Archive.org  
 #	https://archive.org/details/image?tab=collection&query=paleolithic+art+history+slides
-
 	echo "</div>";
 }
 ?>
