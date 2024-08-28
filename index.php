b49eceba044ac6ab16b36ad7929551ca24ae6a3d
rustyhaze2010 <110825165+rustyhaze2010@users.noreply.github.com>
2024-08-28 07:42:32 -0500

    Add files via upload
    Signed-off-by: rustyhaze2010 <110825165+rustyhaze2010@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,33 +1,53 @@
 <html lang="en">
-<doctype html>
+<!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <?php
-session_start();                        /// Starts session variable use.
+##session_start();                        /// Starts session variable use.
 error_reporting ();                     /// NULL turns off reporting.  
 include "common/var/variables.php";     
 ?>
-
 <head>
 
-<title><?php echo "$zSite"; ?>-<?php echo "$zMotto"; ?> - <?php echo "$page"; ?></title>
+<title><?php echo $zSite; ?>-<?php echo $page=$_GET['page']; ?></title>
+
+
 
 <link rel="stylesheet" href="common/css/icing-2022.css">
+<link rel="stylesheet" href="common/css/page-2022.css">
 <link rel="shortcut icon" href="common/media/favicon.png" type="image/x-icon">
-<link rel="icon" href="common/media/favicon.png">
+    <link rel="icon" href="common/media/favicon.png">
     
-<meta charset="utf-8" />
+	<meta charset="utf-8" />
 
-<meta name="keywords" content="<?php echo $zSite; ?>" />
+	<meta name="keywords" content="<?php echo $zSite; ?>" />
 
-<meta name="revised" content="8/2/2022" />
+	<meta name="revised" content="8/2/2022" />
     
 <style>
     body {
-        background-image: url("common/media/ShuttleDiscovery.jpg");
+        background-image: url("common/media/00-wall-grey.jpg");
         background-repeat: no-repeat;
         background-attachment: fixed;
         background-size: cover;
         min-height: 600px;
-    }      
+        font-family: 'Verdana', 'Helvetica', 'Times';
+        background: tan;
+    }
+    #Header{
+        background-image: url("common/media/images/header/welcome-3.jpg");
+    
+    
+    } 
+    #Page, #MasterPage{
+        ##background-image: url("common/media/Discovery.jpg");
+        ##background-image: url("common/media/ShuttleDiscovery.jpg");
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+		background: #4c5e85;
+
+    
+    }
     </style> 
 
 </head>
@@ -36,37 +56,54 @@ include "common/var/variables.php";
 
 <div id="Header">
 
-<?php echo "$zSite"; ?> - <?php echo "$zMotto"; ?>
+
 </div>
 
 
 
 
 <div id="Menu">
-	<image src="common/media/favicon.png" height="25px" width="25px">
-	<a href="index.php?page=welcome">#HelloWorld</a> .  
-	<a href="index.php?page=home">#Home</a> . 
-	<a href="index.php?page=about">#About</a> . 
-	<a href="index.php?page=projects">#Projects</a> . 
-	<a href="http://rustyhaze.net/" target="_parent">#RH</a>
+<img src="common/media/favicon.png" height="25px" width="25px">
+<a href="index.php?page=projects"><?php echo $zSite; ?></a> .  
+
+<a href="index.php?page=about">#About</a> . 
+<a href="index.php?page=artSearch-levelone">#ArtSearch</a> . 
+<a href="index.php?page=video">#Video</a>
+
+
 </div>
 
 <div id="Page">
 <?php
 
-$page = $_GET['page'];
+$zPage = $_GET['page'];
+$zWave = $_GET['wave'];
 
-if (
-	!strpos($page,".")&&
-	!strpos($page,"/")
-)
+if (!strpos($zPage,".")&&!strpos($zPage,"/"))
 {
-if ($page){
-	include_once("page/".$page.".php");
-	}
-	else{
-	include_once('page/welcome.php');
-	}
+	
+if (!strpos($zWave,".")&&!strpos($zWave,"/"))
+{	
+	
+		if ($zPage)
+		{
+		include("page/" . $zPage . ".php");
+		
+		}
+		else
+		{
+		include("page/projects.php");
+		}
+}
+else
+	
+		echo die("Wave used, was broken! Please try again.");
+
+}
+	else
+	
+		echo die("Page used, was broken! Please try again.");
+
 
 ?>
 
@@ -74,11 +111,24 @@ if ($page){
 
 </div>
 <div id="Footer">
+<img src="common/media/favicon.png" height="25px" width="25px">
+<a href="https://rustyhaze.website/index.php?page=projects"><?php echo $zSite; ?></a> .  
 
-	<?php echo $zCopyright; ?>
+<a href="index.php?page=about">#About</a> . 
+<a href="index.php?page=artSearch-levelone">#ArtSearch</a> . 
+<a href="index.php?page=video">#Video</a> . 
 
+<a href="index.php?page=terms">#Terms</a> . 
+<a href="index.php?page=privacy">#Privacy</a> . 
+
+<a href="https://mail.google.com/">#Mail</a> . 
+<a href="index.php?page=search">#Search</a>
+
+<?php echo "<p>" . $zCopyright . "<p>" . $ztick ;?>
 
 </div>
+
 </div>
 </body>
 </html>
+
