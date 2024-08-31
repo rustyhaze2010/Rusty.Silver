3b188c09224670aa6824a82d7cb6803b0ec2a241
jae.lamp <110825165+rustyhaze2010@users.noreply.github.com>
2024-08-31 10:21:21 -0500

    Update variables.php
    Add labels to var page and discriptions.
    
    Signed-off-by: jae.lamp <110825165+rustyhaze2010@users.noreply.github.com>

--- common/var/variables.php
+++ common/var/variables.php
@@ -1,30 +1,30 @@
 <?php 
 //set common components start
-include "timekeeper.php";
+include "timekeeper.php";                                    /// time tasks
 // include "serverinfo.php";
 
 //set common variables
-$zSite = "RustyHaze.website";
-$zAuthor = "Jae";
-$zMotto = "Liv Long and Art Search.";
-$zHome = "home";
-$zCopyright = "Copyright Jae Lamphier. May 1, 2023";
-$zMite = "index.php";
-$zStart = "projects";
-$zMail= "key.9137@outlook.com";
-$zManagerLevel="zAdmin";
+$zSite = "RustyHaze.website";                                /// Site Title or name.
+$zAuthor = "Jae";                                            /// Site Author or owner.
+$zMotto = "Liv Long and Art Search.";                        /// Site Motto.
+$zHome = "home";                                             /// Sets homepage.
+$zCopyright = "Copyright Jae Lamphier. May 1, 2023";         /// Copyright info.
+$zMite = "index.php";                                        /// Main page look back, so you know where home is.
+$zStart = "projects";                                        /// First page to load, or home.
+$zMail= "key.9137@outlook.com";                              /// Main site e-mail address
+$zManagerLevel="zGuest";                                     /// zGuest for guest mode or zAdmin to run in admin mode
 
-//set common components finish
+//set common components finish                               /// adds functions to site.
 include "derailer.php";
 include "funtions-001.php";
 include "funtions-002.php";
 include "funtions-003.php";
 
-if (!isset($_GET["page"])) {                 // page
+if (!isset($_GET["page"])) {                                 /// page arguements
     $_GET["page"] = $zStart;
     }
     
-if (!isset($_GET["wave"])) {                 // wave
+if (!isset($_GET["wave"])) {                                 /// wave arguements
     $_GET["wave"] = "";
     }
    
